d1041684612938a8574d949916b929b0bc67cece
Miguel Flores Jiménez <50829507+m1gu3lfj@users.noreply.github.com>
2019-05-29 20:20:25 +0200

    Add files via upload

---
+++ POJOClass.java
@@ -0,0 +1,52 @@
+package com.examples.java;
+
+public class POJOClass {
+	private int field1;
+	private double field2;
+	private char field3;
+	private boolean field4;
+	private String field5;
+
+	public POJOClass() {
+	}
+	
+	public int getField1() {
+		return field1;
+	}
+
+	public void setField1(int field1) {
+		this.field1 = field1;
+	}
+
+	public double getField2() {
+		return field2;
+	}
+
+	public void setField2(double field2) {
+		this.field2 = field2;
+	}
+
+	public char getField3() {
+		return field3;
+	}
+
+	public void setField3(char field3) {
+		this.field3 = field3;
+	}
+
+	public boolean isField4() {
+		return field4;
+	}
+
+	public void setField4(boolean field4) {
+		this.field4 = field4;
+	}
+
+	public String getField5() {
+		return field5;
+	}
+
+	public void setField5(String field5) {
+		this.field5 = field5;
+	}
+}
